4941cff3c1ca0bb9522656e29f3a0b9a158d6910
nhonnt17 <ngothanhnhonnh@gmail.com>
2025-11-04 21:51:34 +0700

    feat: add new class

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
